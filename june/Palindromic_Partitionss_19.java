e6a3a5712eaadeb1d5d122122be5a47a12c1a9bc
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-19 00:02:12 +0530

    19  jun PODGFG

---
+++ june/Palindromic_Partitionss_19.java
@@ -0,0 +1,40 @@
+package june;
+
+import java.util.ArrayList;
+
+public class Palindromic_Partitionss_19 {
+	 public ArrayList<ArrayList<String>> palinParts(String s) {
+        // code here
+        ArrayList<ArrayList<String>> res = new ArrayList<>();
+        backtrack(s,0,new ArrayList<>(),res);
+        return res;
+    }
+    private void backtrack(String s,int idx,
+    ArrayList<String> curr,ArrayList<ArrayList<String>> res)
+    {
+        if(idx==s.length())
+        {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=idx;i<s.length();i++)
+        {
+            if(isPalin(s,idx,i))
+            {
+                curr.add(s.substring(idx,i+1));
+                backtrack(s,i+1,curr,res);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    private boolean isPalin(String s,int low,int high)
+    {
+        while(low<high)
+        {
+            if(s.charAt(low++)!=s.charAt(high--))
+            return false;
+        }
+        return true;
+    }
+ 
+}
