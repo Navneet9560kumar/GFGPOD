fa0ada08ba9910a2b93efa71a992975a90791d02
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-21 23:42:38 +0530

    PODGFG-21-JUNE

--- june/policeandthieves_21.java
+++ june/policeandthieves_21.java
@@ -6,21 +6,21 @@ public class policeandthieves_21 {
 
        public int catchThieves(char[] arr, int k) {
         // code here
-        Queue<Integer> police=new LinkedList<>(),thieves=new LinkedList<>();
-        int caught=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=='P') police.offer(i);
+        Queue<Integer>police=  new LinkedList<>(),thieves= new LinkedList<>();
+        int cought=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]=='p') police.offer(i);
             else thieves.offer(i);
         }
-        while(!police.isEmpty() && !thieves.isEmpty()){
+        while (!police.isEmpty() && !thieves.isEmpty()) {
             if(Math.abs(police.peek()-thieves.peek())<=k){
-                caught++;
-                police.poll();
-                thieves.poll();
-            }else if(police.peek()<thieves.peek()) police.poll();
+                  cought++;
+                  police.peek();
+                  thieves.poll();
+            }else if(police.peek()<thieves.peek())police.poll();
             else thieves.poll();
         }
-        return caught;
+        return cought;
     }
       public static void main(String[] args) {
             int[]arr = {'p','t','p'};
