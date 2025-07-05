2e9bbf11f905de5a3d22e2187e269bdff629f6c1
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-07-06 01:32:24 +0530

    GFGPOD-06/07/2025

---
+++ jul/MaxScorefromSubarrayMins.java
@@ -0,0 +1,17 @@
+package jul;
+
+public class MaxScorefromSubarrayMins {
+       public int maxSum(int arr[]) {
+        // code here
+        int n = arr.length;
+        int ans = 0;
+        for(int i=0;i<n-1;i++){
+            int sum = arr[i]+arr[i+1];
+            ans = Math.max(ans,sum);
+        }
+        return ans;
+    }
+      public static void main(String[] args) {
+            
+      }
+}
