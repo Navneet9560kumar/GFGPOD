e43c9b8b20072f69d54db8f80b70928d38a829da
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-07-18 03:14:51 +0530

    GFG pod-18-jul

---
+++ jul/NineDisiver.java
@@ -0,0 +1,34 @@
+package jul;
+
+public class NineDisiver {
+
+      public static int count(int n){
+            int i=2;
+            int ans =3;
+            int num = n*n;
+            for(int j=i;j<n;j++){
+                  if(num%j==0)ans+=2;
+                  if(ans>9)return ans;
+            }
+            return ans;
+      }
+
+public static  int countNumber(int n){
+         
+            if(n<36) return 0;
+
+            int i = 6;
+            int ans = 0;
+            while((i*i)<n){
+                  if(count(i)==9){
+                        ans++;
+                  }
+                  i++;
+            }
+            return ans;
+}
+      public static void main(String[] args) {
+          
+
+      }
+}
