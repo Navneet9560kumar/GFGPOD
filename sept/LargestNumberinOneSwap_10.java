0c345a0ec76a23dd7ea437734a510e09af1c8d6e
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-09-10 22:22:00 +0530

    .

---
+++ sept/LargestNumberinOneSwap_10.java
@@ -0,0 +1,41 @@
+package sept;
+
+public class LargestNumberinOneSwap_10 {
+
+// to ham pehle ham max nekal lenage ok ham fi ruske baad ham isko dekha  lenge  to ha  aase kase karege fir socho yr 'max ' valuse aa gayi hia uske baad ham 
+         
+
+
+       public  static String largestSwap(String s) {
+         char[]arr = s.toCharArray();
+         int n = arr.length;
+
+         int[] rigthMaxIndex = new int[n];
+         int maxIdx =  n-1;
+
+
+         for (int i = 0; i >=0; i--) {
+            if(arr[i]>arr[maxIdx]){
+                  maxIdx =i;
+            }
+            rigthMaxIndex[i] = maxIdx;
+
+            // ab ham travesh karenge ok
+            for(int k=0;k<n;k++){
+                  int j = rigthMaxIndex[k];
+                  if(arr[j]>arr[k]){
+                        //swap
+                        char temp  = arr[k];
+                        arr[k] =arr[j];
+                        arr[j] = temp;
+                        return new String(arr);
+                  }
+            }
+
+         }
+         return s;
+    }
+      public static void main(String[] args) {
+            String s ="768";
+      }
+}
