3d036d9b2be968e8060802591b181a842c45b5f4
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-12-21 16:05:20 +0530

    .

---
+++ Dec/colwishtravesh.java
@@ -0,0 +1,60 @@
+package Dec;
+
+public class colwishtravesh {
+
+      public static int col(String[] strs) {
+            int rows = strs.length;
+            int cols = strs[0].length(); // sab strings same length ki maan rahe hain
+            int count = 0;
+
+            // String[] columns = new String[cols];
+
+            // for (int j = 0; j < cols; j++) {
+            //       StringBuilder sb = new StringBuilder();
+
+            //       for (int i = 0; i < rows; i++) {
+            //             sb.append(strs[i].charAt(j));
+            //       }
+
+            //       columns[j] = sb.toString();
+
+
+            
+
+            // Print result
+            // for (String col : columns) {
+            // System.out.println(col);
+            // }
+
+
+            for(int j=0;j<cols;j++){
+                  for(int i=0; i<rows;i++){
+                  if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+                        count++;
+                  }
+            }
+         
+      }
+         return count;
+}
+
+      
+
+      public int minDeletionSize(String[] strs) {
+
+      }
+
+      public static void main(String[] args) {
+
+      }
+
+}
+
+
+//strs = ["abc", "bce", "cae"] 
+// a  b  c
+// b  c  a
+// c  e  e
+// ↑  ↑  ↑
+// |  |  |
+// abc bca cee
