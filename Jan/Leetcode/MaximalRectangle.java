6cea78f7894a6a93ae60c0874cb3fc3b277c79de
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2026-01-12 00:18:10 +0530

    LL revision

---
+++ Jan/Leetcode/MaximalRectangle.java
@@ -0,0 +1,62 @@
+package Jan.Leetcode;
+
+import java.util.Stack;
+
+public class MaximalRectangle {
+
+       public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for (char[] row : matrix) {
+
+          
+            for (int j = 0; j < cols; j++) {
+                if (row[j] == '1')
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            maxArea = Math.max(maxArea, largestRectangle(heights));
+        }
+        return maxArea;
+    }
+
+    // Largest rectangle dekhte hai ham ok 
+public static int largestRectangle(int[] heights) {
+
+    Stack<Integer> st = new Stack<>();
+    int maxArea = 0;
+    int n = heights.length;
+
+    for (int i = 0; i <= n; i++) {
+
+        int currHeight = (i == n) ? 0 : heights[i];
+
+        while (!st.isEmpty() && currHeight < heights[st.peek()]) {
+
+            int height = heights[st.pop()];
+            int width;
+
+            if (st.isEmpty())
+                width = i;
+            else
+                width = i - st.peek() - 1;
+
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        st.push(i);
+    }
+
+    return maxArea;
+}
+
+      public static void main(String[] args) {
+
+      }
+}
